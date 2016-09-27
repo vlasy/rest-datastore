4764002655d203ed3c2e3581b60285c259293bc5
Michal Vlasák <daeatel@gmail.com>
2016-09-27 03:46:39 +0200

    First working version of datastore with tests

--- rest_datastore/datastore.py
+++ rest_datastore/datastore.py
@@ -1,25 +1,143 @@
+import requests
 from flask_security.datastore import Datastore, UserDatastore
+from flask_security.utils import get_identity_attributes
+from flask.ext.security import UserMixin, RoleMixin
+
+
+class RESTModel(dict):
+    url = None
+
+    def __getattr__(self, key):
+        return self[key]
+
+
+class RoleModel(RESTModel, RoleMixin):
+    url = "role"
+
+
+class UserModel(RESTModel, UserMixin):
+    url = "user"
+
+    def has_role(self, role):
+        """Returns `True` if the user identifies with the specified role.
+
+        :param role: A role name or `Role` instance"""
+        if isinstance(role, str):
+            return role in (role['name'] for role in self.roles)
+        else:
+            return role in self.roles
 
 
 class RESTDatastore(Datastore):
 
+    def __init__(self, api_url, db=None):
+        Datastore.__init__(self, db)
+        self._url = api_url
+
+    def get(self, model, params):
+        result = requests.get(self._url + '/' + model.url, params=params)
+        if result.json()['_meta']['total'] > 0:
+            return model(result.json()['_items'][0])
+        return None
+
+    def _prepare_role_modify_args(self, user, role):
+        if isinstance(user, str):
+            user = self.find_user(email=user)
+        if isinstance(role, str):
+            role = self.find_role(name=role)
+        return user, role
+
+    def add_role_to_user(self, user, role):
+        """Adds a role to a user.
+
+        :param user: The user to manipulate
+        :param role: The role to add to the user
+        """
+        user, role = self._prepare_role_modify_args(user, role)
+        if role not in user.roles:
+            # update online repre
+            requests.post(self._url + '/user/' + str(user['id']) +
+                          '/roles', json=role)
+            # update local repre
+            user.roles.append(role)
+            return True
+        return False
+
+    def remove_role_from_user(self, user, role):
+        """Removes a role from a user.
+
+        :param user: The user to manipulate
+        :param role: The role to remove from the user
+        """
+        rv = False
+        user, role = self._prepare_role_modify_args(user, role)
+        if role.id in (role['id'] for role in user.roles):
+            rv = True
+            # update online repre
+            requests.delete(self._url + '/user/' + str(user['id']) +
+                            '/roles', json=role)
+            # update local repre
+            user.roles = [x for x in user.roles if x['id'] != role['id']]
+        return rv
+
     def put(self, model):
-        raise NotImplementedError
+        backup_roles = None
+        if 'roles' in model.keys():
+            backup_roles = list(model['roles'])
+            model['roles'] = []  # prevent roles from being sent with user
+        res = requests.post(self._url + '/' + model.url, json=model)
+        model['id'] = res.json()['id']
+        if backup_roles:
+            for role in backup_roles:
+                self.add_role_to_user(model, role)
+        model['roles'] = backup_roles    # give roles back
+        return model
 
     def delete(self, model):
-        raise NotImplementedError
+        requests.delete(self._url + '/' + model.url, json=model)
 
 
 class RESTUserDatastore(RESTDatastore, UserDatastore):
 
+    def __init__(self, api_url):
+        RESTDatastore.__init__(self, api_url)
+        UserDatastore.__init__(self, UserModel, RoleModel)
+
     def get_user(self, id_or_email):
         """Returns a user matching the specified ID or email address."""
-        raise NotImplementedError
+        if (isinstance(id_or_email, int)):  # id
+            result = requests.get(self._url + '/' + UserModel.url +
+                                  '/' + str(id_or_email))
+            return UserModel(result.json())
+        else:   # email
+            for a in get_identity_attributes():
+                # this will pass kwargs explicitly
+                u = self.find_user(**{a: id_or_email})
+                if u:
+                    return u
+            return None
 
     def find_user(self, *args, **kwargs):
         """Returns a user matching the provided parameters."""
-        raise NotImplementedError
+        # TODO: merge it with solution from find_role so you can use
+        # both args and kwargs - something like dict update or merge
+        s = ""
+        # print("find_user method")
+        for key, val in kwargs.items():
+            s += str(key) + "==\"" + str(val) + "\""
+        query = {"where": s}
+        # print(query)
+        return self.get(UserModel, query)
 
     def find_role(self, *args, **kwargs):
         """Returns a role matching the provided name."""
-        raise NotImplementedError
+        name = None
+        if len(args) > 0:
+            name = args[0]
+        if not name and 'name' in kwargs.keys():
+            name = kwargs['name']
+        if not name:
+            return
+        # TODO: ok - this just works but its ugly!
+        query = {"where": "name==" + name}
+        return self.get(RoleModel, query)
--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -6,11 +6,152 @@
     REST Datastore tests
 """
 
+import requests
 from pytest import fixture
-
+from flask import Flask
+from flask_security import Security
+from flask_security.utils import encrypt_password
 from rest_datastore import RESTUserDatastore
 
 
 @fixture()
-def datastore(request, app, tmpdir):
-    return RESTUserDatastore()
+def datastore():
+    return RESTUserDatastore("http://localhost:5000")
+
+
+def create_roles(ds):
+    for role in ('admin', 'editor', 'author'):
+        ds.create_role(name=role)
+
+
+def create_users(ds, count=None):
+    users = [('matt@lp.com', 'matt', 'password', ['admin'], True),
+             ('joe@lp.com', 'joe', 'password', ['editor'], True),
+             ('dave@lp.com', 'dave', 'password', ['admin', 'editor'], True),
+             ('jill@lp.com', 'jill', 'password', ['author'], True),
+             ('tiya@lp.com', 'tiya', 'password', [], False),
+             ('jess@lp.com', 'jess', None, [], True)]
+    count = count or len(users)
+
+    for u in users[:count]:
+        pw = u[2]
+        if pw is not None:
+            pw = encrypt_password(pw)
+        roles = [ds.find_or_create_role(rn) for rn in u[3]]
+        ds.commit()
+        user = \
+            ds.create_user(email=u[0], username=u[1], password=pw, active=u[4])
+        ds.commit()
+        for role in roles:
+            ds.add_role_to_user(user, role)
+        ds.commit()
+
+
+def populate_data(app, user_count=None):
+    ds = app.security.datastore
+    with app.app_context():
+        create_roles(ds)
+        create_users(ds, user_count)
+
+
+def purge_data(app):
+    requests.delete('http://localhost:5000/role')
+    requests.delete('http://localhost:5000/user')
+
+
+@fixture()
+def app(request):
+    app = Flask(__name__)
+    app.debug = True
+    app.config['SECRET_KEY'] = 'secret'
+    app.config['TESTING'] = True
+    app.config['LOGIN_DISABLED'] = False
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    for opt in ['changeable', 'recoverable', 'registerable',
+                'trackable', 'passwordless', 'confirmable']:
+        app.config['SECURITY_' + opt.upper()] = opt in request.keywords
+
+    if 'settings' in request.keywords:
+        for key, value in request.keywords['settings'].kwargs.items():
+            app.config['SECURITY_' + key.upper()] = value
+    return app
+
+
+def init_app_with_options(app, datastore, **options):
+    security_args = options.pop('security_args', {})
+    app.config.update(**options)
+    app.security = Security(app, datastore=datastore, **security_args)
+    purge_data(app)
+    populate_data(app)
+
+
+def test_get_user(app, datastore):
+    init_app_with_options(app, datastore, **{
+        'SECURITY_USER_IDENTITY_ATTRIBUTES': ('email', 'username')
+    })
+
+    with app.app_context():
+        user_id = datastore.find_user(email='matt@lp.com').id
+
+        user = datastore.get_user(user_id)
+        assert user is not None
+
+        user = datastore.get_user('matt@lp.com')
+        assert user is not None
+
+        user = datastore.get_user('matt')
+        assert user is not None
+
+
+def test_find_role(app, datastore):
+    init_app_with_options(app, datastore)
+
+    role = datastore.find_role('admin')
+    assert role is not None
+
+    role = datastore.find_role('bogus')
+    assert role is None
+
+
+def test_add_role_to_user(app, datastore):
+    init_app_with_options(app, datastore)
+
+    # Test with user object
+    user = datastore.find_user(email='matt@lp.com')
+    assert user.has_role('editor') is False
+    assert datastore.add_role_to_user(user, 'editor') is True
+    assert datastore.add_role_to_user(user, 'editor') is False
+    assert user.has_role('editor') is True
+
+    # Test with email
+    assert datastore.add_role_to_user('jill@lp.com', 'editor') is True
+    user = datastore.find_user(email='jill@lp.com')
+    assert user.has_role('editor') is True
+
+    # Test remove role
+    assert datastore.remove_role_from_user(user, 'editor') is True
+    assert datastore.remove_role_from_user(user, 'editor') is False
+
+
+def test_create_user_with_roles(app, datastore):
+    init_app_with_options(app, datastore)
+
+    role = datastore.find_role('admin')
+    datastore.commit()
+
+    user = datastore.create_user(email='dude@lp.com', username='dude',
+                                 password='password', roles=[role])
+    datastore.commit()
+    user = datastore.find_user(email='dude@lp.com')
+    assert user.has_role('admin') is True
+
+
+def test_delete_user(app, datastore):
+    init_app_with_options(app, datastore)
+
+    user = datastore.find_user(email='matt@lp.com')
+    datastore.delete_user(user)
+    datastore.commit()
+    user = datastore.find_user(email='matt@lp.com')
+    assert user is None
